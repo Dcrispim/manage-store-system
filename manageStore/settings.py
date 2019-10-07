e483e54cf5b50877f3cd6e4d7d21b80df6d7cdc5
Dcrispim <diego.crispim@yahoo.com.br>
2019-10-07 12:48:38 -0300

    Fix STATIC_ROOT 2

--- manageStore/settings.py
+++ manageStore/settings.py
@@ -124,8 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+#STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
+STATICFILES_DIRS = ['static']
 
 LOGIN_REDIRECT_URL = '/'
 
