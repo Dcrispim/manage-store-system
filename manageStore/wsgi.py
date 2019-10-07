a194b9a21094358dca5fa0f430be5696a4d7d0e6
Dcrispim <diego.crispim@yahoo.com.br>
2019-10-07 12:33:59 -0300

    change Clint to Cling

--- manageStore/wsgi.py
+++ manageStore/wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Clint
+from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manageStore.settings')
 
-application = Clint(get_wsgi_application())
+application = Cling(get_wsgi_application())
