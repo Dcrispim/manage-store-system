7e089f3b43cc78c9a0df08d2c646a4b04b96736e
Dcrispim <diego.crispim@yahoo.com.br>
2019-10-08 09:25:13 -0300

    Removed login_required decorator from _functions

--- operations/views.py
+++ operations/views.py
@@ -41,7 +41,7 @@ def salebuy(request):
             pkId = i['product']
 
         ID = Product.objects.get(pk=pkId)
-        addCartItem(pkId, i['qtd'], OPID, 0)
+        _addCartItem(pkId, i['qtd'], OPID, 0)
 
     def updateStock(id, qtd):
         Stock.objects.filter(pk=id).update(qtd=qtd)
@@ -103,11 +103,11 @@ def salebuy(request):
                 off = float(request.POST['off'])/100
                 if form['mode'].value() == '0':
                     total = addVenda(opid)
-                    addOperation({'description': f'IDV{ opid.pk }',
+                    _addOperation({'description': f'IDV{ opid.pk }',
                                   'orig_dest': f'{opid.client}', 'credit': total, 'debt': 0, 'status': opid.status, 'date': opid.date, 'off': off})
                 elif form['mode'].value() == '1':
                     total = addCompra(opid)
-                    addOperation({'description': f'IDC{ opid.pk }',
+                    _addOperation({'description': f'IDC{ opid.pk }',
                                   'orig_dest': f'{opid.client}', 'credit': 0, 'debt': total, 'status': opid.status, 'date': opid.date, 'off': off})
                 setList()
                 return redirect('salebuy')
@@ -182,11 +182,11 @@ def service(request):
             tp = 0 if type_item == 'items' else 1
             try:
 
-                if stockVerify(item['product'], item['qtd']):
+                if _stockVerify(item['product'], item['qtd']):
                     prod = Product.objects.get(pk=item['product'])
                     stockItem = Stock.objects.filter(product=prod)[0]
-                    if stockSub(stockItem.pk, item['qtd']) > -1:
-                        addCartItem(item['product'],item['qtd'],opid.pk, op_type)
+                    if _stockSub(stockItem.pk, item['qtd']) > -1:
+                        _addCartItem(item['product'],item['qtd'],opid.pk, op_type)
                         totalitems += stockItem.sale_price*item['qtd']
             except Exception as err:
                 print(err)
@@ -203,7 +203,7 @@ def service(request):
         total_materials = addServiceItems('materials', svId)
         total = total_items + total_materials + svId.labor
         print(total_items, total_materials, total)
-        addOperation({'description': f'IDS{ svId.pk }',
+        _addOperation({'description': f'IDS{ svId.pk }',
                       'orig_dest': f'{svId.client}',
                       'credit': total,
                       'debt': 0,
@@ -255,15 +255,15 @@ def operation(request):
     
     return render(request, 'operation.html', data)
 
-@login_required
-def addOperation(item):
+
+def _addOperation(item):
     Operation.objects.create(description=item['description'],
                              orig_dest=item['orig_dest'], credit=float(item['credit'])*(1-float(item['off'])), debt=float(item['debt'])*(1-float(item['off'])),
                              status=item['status'], date=item['date'],
                              )
 
-@login_required
-def stockVerify(productId, qtd):
+
+def _stockVerify(productId, qtd):
     product = Product.objects.get(pk=productId) if len(
         Product.objects.filter(pk=productId)) else None
     stockItem = Stock.objects.filter(product=product)[0] if len(
@@ -273,8 +273,8 @@ def stockVerify(productId, qtd):
     else:
         return False
 
-@login_required
-def stockSub(stockId, qtd):
+
+def _stockSub(stockId, qtd):
     stockItem = Stock.objects.get(pk=stockId)
     if stockItem.qtd < qtd:
         return -1
@@ -283,8 +283,8 @@ def stockSub(stockId, qtd):
         Stock.objects.filter(pk=stockId).update(qtd=new_qtd)
         return new_qtd
 
-@login_required
-def addCartItem(prod_id, qtd, op_id, op_type):
+
+def _addCartItem(prod_id, qtd, op_id, op_type):
     op_types = {0: 'COMPRA', 1: 'VENDA', 2: 'ITEM', 3: 'MATERIAL'}
     try:
         ID = Product.objects.get(pk=prod_id)
