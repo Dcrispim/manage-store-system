1724f7ea096159483531616d7161425af0856ed2
Dcrispim <diego.crispim@yahoo.com.br>
2019-11-11 09:51:47 -0300

    split back and fron end

--- frontend/src/common/layout/IconButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default props => (
-    <button type={`${props.type ? props.type: 'button'}`} className={`btn btn-${props.btn}`} onClick={props.onClick}>
-        <i className={`fa fa-${props.icon}`}/>
-    </button>
-)
--- frontend/src/common/layout/Row.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-const Row = (props) => <div className="row">{props.children}</div>;
-
-export default Row;
-
